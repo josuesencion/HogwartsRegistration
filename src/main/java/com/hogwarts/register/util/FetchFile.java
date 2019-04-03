e47ef75e94e3d22199676536ed75f286144d273e
Josue Sencion <josue.sencion1@gmail.com>
2019-04-03 18:53:48 -0400

    The fetchStudents method was fixed so it does not skip the odd-numbered entries.

--- src/main/java/com/hogwarts/register/util/FetchFile.java
+++ src/main/java/com/hogwarts/register/util/FetchFile.java
@@ -1,7 +1,9 @@
 package com.hogwarts.register.util;
 
-import java.io.*;
-import java.io.File;
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.LinkedList;
 import java.util.List;
 
@@ -14,35 +16,18 @@ public class FetchFile {
         List<String[]> students = new LinkedList<>();
         try {
             BufferedReader bufferedReader = new BufferedReader(new FileReader(studentList));
-            while (bufferedReader.readLine() != null) {
-                students.add(bufferedReader.readLine().split(","));
+            String entry;
+            while ((entry = bufferedReader.readLine()) != null) {
+                students.add(entry.split(","));
+                System.out.println();
             }
         } catch (FileNotFoundException e) {
             e.printStackTrace();
         } catch (IOException e) {
-            e.printStackTrace();
+            //e.printStackTrace();
+            System.err.println("An IOException was caught :"+e.getMessage());
         }
         return students;
     }
 
-    /*
-    public List<String[]> fetchNewWizards(String studentList){
-        List<String[]> students = new LinkedList<>();
-
-        ClassLoader classLoader = getClass().getClassLoader();
-        File file = new File(classLoader.getResource(studentList).getFile());
-
-        try {
-            BufferedReader bufferedReader = new BufferedReader(new FileReader(file));
-            while (bufferedReader.readLine() != null) {
-                students.add(bufferedReader.readLine().split(","));
-            }
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        return students;
-    }
-    */
 }
